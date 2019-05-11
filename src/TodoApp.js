93b98bb28a7941fa7f0eec6c1c3ef19a0572ae57
ringosingson <ringosingson@live.com>
2019-05-12 00:39:36 +0530

    First commit

---
+++ src/TodoApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import TodoList from "./TodoList";
+
+export default function TodoApp() {
+  const initialTodos = [
+    {
+      id: 1,
+      task: "Brush your Teeth",
+      completed: false
+    },
+    {
+      id: 2,
+      task: "Wash your Face",
+      completed: true
+    }
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa"
+      }}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList todos={todos} />
+    </Paper>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TodoApp from "./TodoApp";
 
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <TodoApp />;
 }
 
 const rootElement = document.getElementById("root");
