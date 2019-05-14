424006c40ac0c8090fd1cd4aa6f82c7c52d7fa7b
ringosingson <ringosingson@live.com>
2019-05-14 16:26:43 +0530

    toggle and deletion

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,6 +6,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
+import uuid from "uuid";
 
 export default function TodoApp() {
   const initialTodos = [
@@ -27,8 +28,22 @@ export default function TodoApp() {
   ];
   const [todos, setTodos] = useState(initialTodos);
   const addTodo = newTodoText => {
-    setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+    setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
   };
+
+  const removeTodo = todoId => {
+    //filter out removed todo
+    const updatedTodos = todos.filter(todo => todo.id !== todoId);
+    //call setTodos with new todos array
+    setTodos(updatedTodos);
+  };
+  const toggleTodo = todoId => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
   return (
     <Paper
       style={{
@@ -46,7 +61,11 @@ export default function TodoApp() {
       <Grid container justify="center" style={{ marginTop: "1rem" }}>
         <Grid item xs={11} md={8} lg={4}>
           <TodoForm addTodo={addTodo} />
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+          />
         </Grid>
       </Grid>
     </Paper>
